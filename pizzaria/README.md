d42756b6e7b120e7ded9cb6ff03fa96d622341e1
Andrey Kaiky Reis Ferreira <Andrey.kaiky@outlook.com>
2025-04-06 12:32:55 -0300

    Adicionando primeiro projeto p/ estudos na linguagem Go - Em desenvolvimento.

---
+++ pizzaria/README.md
@@ -0,0 +1,1 @@
+# Primeira Aplicação em Go!
---
+++ pizzaria/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type Pizza struct {
+	ID    int
+	nome  string
+	preco float64
+}
+
+func main() {
+	var nomePizzaria string = "Pizzaria Go"
+	// var toscana Pizza
+	instagram, telefone := "@pizzaria_go", 11951
+	fmt.Println(nomePizzaria, instagram, telefone)
+}
